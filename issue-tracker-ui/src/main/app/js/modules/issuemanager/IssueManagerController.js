270cf9c0ad437e831d1ebf8dd5f6090dd7b84581
Jose Diaz <jose.diaz@joedayz.pe>
2015-06-20 13:57:29 -0500

    IssueManagerController 1.0

--- issue-tracker-ui/src/main/app/js/modules/issuemanager/IssueManagerController.js
+++ issue-tracker-ui/src/main/app/js/modules/issuemanager/IssueManagerController.js
@@ -2,5 +2,46 @@ IssueTrackerApp.module('IssueManager',
 	function(IssueManager, IssueTrackerApp, Backbone, Marionette, $, _) {
 
 
+// Define the AppRouter for the IssueManager module
+  var IssueManagerRouter = Marionette.AppRouter.extend({
+
+    appRoutes: {
+      'issues': 'list'  //http://host[:port]/[path/]page.html[#hash][?param=value]
+    }
+
+
+  });
+
+  var IssueManagerController = Marionette.Controller.extend({
+
+  	  list: function(collection) {
+      	logger.debug("IssueManagerController.list");
+
+      	var displayListView = function(issueCollection) {
+        	var listView = new IssueManager.IssueListView({
+          	collection: issueCollection
+        	});
+
+        	logger.debug("Show IssueListView in IssueTrackerApp.mainRegion");
+        	
+			IssueTrackerApp.mainRegion.show(listView);
+		};
+		
+		if(collection){
+			displayListView(collection);
+		}else{
+			var fetchingIssues = IssueTrackerApp.request('issue:entities');
+			$.when(fetchingIssues).done(function(issues) {
+          		displayListView(issues);
+        	});
+		}
+
+      }
+
+  });
+
+  //crear la instancia
+  var controller = new IssueManagerController();
+
 });
 
