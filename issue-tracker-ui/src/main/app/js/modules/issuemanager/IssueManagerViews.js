22ea5d3ce6126af6cb7fb526413fcdab5a3d0360
Jose Diaz <jose.diaz@joedayz.pe>
2015-06-20 13:46:34 -0500

    IssueManagerViews.js 1.0

--- issue-tracker-ui/src/main/app/js/modules/issuemanager/IssueManagerViews.js
+++ issue-tracker-ui/src/main/app/js/modules/issuemanager/IssueManagerViews.js
@@ -2,4 +2,38 @@ IssueTrackerApp.module('IssueManager',
 	function(IssueManager, IssueTrackerApp, Backbone, Marionette, $, _) {
 
 
+  // Define the View for an empty List of Issues
+  IssueManager.IssueListEmptyView = Backbone.Marionette.ItemView.extend({
+
+    tagName: 'tr',
+
+    template: 'issuelistempty'
+
+  });
+
+  // Define the View for a single Issue in the List
+  IssueManager.IssueListItemView = Backbone.Marionette.ItemView.extend({
+
+    tagName: 'tr',
+
+    template: 'issuelistitem'
+
+	});
+
+ 	// Define the View for a List of Issues
+  IssueManager.IssueListView = Backbone.Marionette.CompositeView.extend({
+
+	emptyView: IssueManager.IssueListEmptyView,
+
+    childView: IssueManager.IssueListItemView,
+
+	childViewContainer: 'tbody',
+
+	className: 'container-fluid',
+
+	template: 'issueList'
+
+
+	});
+
 });
