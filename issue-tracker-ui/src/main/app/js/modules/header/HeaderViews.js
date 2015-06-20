20512dba8b88c6657c9e87e125db628eb27be79b
Jose Diaz <jose.diaz@joedayz.pe>
2015-06-20 11:43:54 -0500

    soportando namespace IssueTrackerTemplates

---
+++ issue-tracker-ui/src/main/app/js/modules/header/HeaderViews.js
@@ -0,0 +1,11 @@
+IssueTrackerApp.module('Header',
+ function(Header, IssueTrackerApp, Backbone, Marionette, $, _) {
+
+ // Define the NavBarView Class
+ Header.NavBarView = Backbone.Marionette.ItemView.extend({
+
+ template: 'navbar'
+
+ });
+
+ });
