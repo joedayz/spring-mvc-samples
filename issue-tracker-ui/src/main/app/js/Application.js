3da098b078493638a16c0bd9391b692b0ed3f494
Jose Diaz <jose.diaz@joedayz.pe>
2015-06-20 11:00:44 -0500

    issue tracker ui

---
+++ issue-tracker-ui/src/main/app/js/Application.js
@@ -0,0 +1,47 @@
+//Creamos la aplicacion
+
+window.IssueTrackerApp = new Backbone.Marionette.Application();
+
+//Crear las 4 regiones
+
+IssueTrackerApp.addRegions({
+	headerRegion : '#header-region',
+	mainRegion : '#main-region',
+	dialogRegion : '#dialog-region',
+	footerRegion : '#footer-region',
+});
+
+// Eventos de la aplicacion
+//Inicializar el ruteador
+
+IssueTrackerApp.on('start', function(options) {
+	logger.debug("Backbone.history.start");
+	Backbone.history.start();
+});
+
+//Iniciar la aplicacion
+$( function(){
+	//Configurar la libreria log4javascript
+	window.logger = log4javascript.getLogger();
+	consoleAppender = new log4javascript.BrowserConsoleAppender();
+	consoleAppender.setLayout(new log4javascript.PatternLayout('%d{HH:mm:ss} %-5p- %m'));
+	window.logger.addAppender(consoleAppender);
+
+	//inicias la aplicacion Marionette
+	logger.debug("IssueTrackerApp.start");
+	IssueTrackerApp.start();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
