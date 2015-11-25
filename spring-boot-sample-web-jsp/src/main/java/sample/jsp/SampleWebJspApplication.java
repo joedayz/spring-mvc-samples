4f13e67b1bc1deafd8b43da690aa9267f457d0d9
Jose Diaz <jose.diaz@joedayz.pe>
2015-11-25 14:25:27 -0500

    Issue UI Boot 2.0

--- spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
+++ spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+
 @SpringBootApplication
 public class SampleWebJspApplication extends SpringBootServletInitializer {
 
