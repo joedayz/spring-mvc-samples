94ccc08879cb9ce24e2afd2ab2c7529666e44c85
Jose Diaz <jose.diaz@joedayz.pe>
2016-08-05 21:29:18 -0500

    update to 1.4.0.RELEASE

--- issue-tracker-ws/src/main/java/org/example/issuetracker/Application.java
+++ issue-tracker-ws/src/main/java/org/example/issuetracker/Application.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableMicroservicesDashboardServer
 public class Application {
 
 	public static void main(String[] args) {
