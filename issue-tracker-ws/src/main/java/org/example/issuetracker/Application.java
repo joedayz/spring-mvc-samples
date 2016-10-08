d3b7b1a473b8933c58b0b374f48ab8713767f934
Jose Diaz <jose.diaz@joedayz.pe>
2016-10-08 13:00:02 -0500

    eliminando dependencias

--- issue-tracker-ws/src/main/java/org/example/issuetracker/Application.java
+++ issue-tracker-ws/src/main/java/org/example/issuetracker/Application.java
@@ -1,17 +1,10 @@
 package org.example.issuetracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-@EnableMicroservicesDashboardServer
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
